3af1022485679d5e9d10891e335a2fa785835413
Tom Gehrke <tom.gehrke@sap.com>
2025-10-22 10:13:55 +0200

    Replace deprecated docker types
    Signed-off-by: Tom Gehrke <tom.gehrke@sap.com>

--- sdk/helper/docker/testhelpers.go
+++ sdk/helper/docker/testhelpers.go
@@ -21,7 +21,7 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
-	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/build"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/image"
@@ -29,10 +29,10 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/go-connections/nat"
 	"github.com/hashicorp/go-uuid"
+	"github.com/moby/go-archive"
 )
 
 const DockerAPIVersion = "1.40"
@@ -339,12 +339,12 @@ func (d *Runner) StartNewService(ctx context.Context, addSuffix, forceLocalAddr 
 type Service struct {
 	Config      ServiceConfig
 	Cleanup     func()
-	Container   *types.ContainerJSON
+	Container   *container.InspectResponse
 	StartResult *StartResult
 }
 
 type StartResult struct {
-	Container *types.ContainerJSON
+	Container *container.InspectResponse
 	Addrs     []string
 	RealIP    string
 }
@@ -770,14 +770,14 @@ func (bCtx *BuildContext) ToTarball() (io.Reader, error) {
 }
 
 type BuildOpt interface {
-	Apply(cfg *types.ImageBuildOptions) error
+	Apply(cfg *build.ImageBuildOptions) error
 }
 
 type BuildRemove bool
 
 var _ BuildOpt = (*BuildRemove)(nil)
 
-func (u BuildRemove) Apply(cfg *types.ImageBuildOptions) error {
+func (u BuildRemove) Apply(cfg *build.ImageBuildOptions) error {
 	cfg.Remove = bool(u)
 	return nil
 }
@@ -786,7 +786,7 @@ type BuildForceRemove bool
 
 var _ BuildOpt = (*BuildForceRemove)(nil)
 
-func (u BuildForceRemove) Apply(cfg *types.ImageBuildOptions) error {
+func (u BuildForceRemove) Apply(cfg *build.ImageBuildOptions) error {
 	cfg.ForceRemove = bool(u)
 	return nil
 }
@@ -795,7 +795,7 @@ type BuildPullParent bool
 
 var _ BuildOpt = (*BuildPullParent)(nil)
 
-func (u BuildPullParent) Apply(cfg *types.ImageBuildOptions) error {
+func (u BuildPullParent) Apply(cfg *build.ImageBuildOptions) error {
 	cfg.PullParent = bool(u)
 	return nil
 }
@@ -804,7 +804,7 @@ type BuildArgs map[string]*string
 
 var _ BuildOpt = (*BuildArgs)(nil)
 
-func (u BuildArgs) Apply(cfg *types.ImageBuildOptions) error {
+func (u BuildArgs) Apply(cfg *build.ImageBuildOptions) error {
 	cfg.BuildArgs = u
 	return nil
 }
@@ -813,7 +813,7 @@ type BuildTags []string
 
 var _ BuildOpt = (*BuildTags)(nil)
 
-func (u BuildTags) Apply(cfg *types.ImageBuildOptions) error {
+func (u BuildTags) Apply(cfg *build.ImageBuildOptions) error {
 	cfg.Tags = u
 	return nil
 }
@@ -825,7 +825,7 @@ func (d *Runner) BuildImage(ctx context.Context, containerfile string, container
 }
 
 func BuildImage(ctx context.Context, api *client.Client, containerfile string, containerContext BuildContext, opts ...BuildOpt) ([]byte, error) {
-	var cfg types.ImageBuildOptions
+	var cfg build.ImageBuildOptions
 
 	// Build container context tarball, provisioning containerfile in.
 	containerContext[containerfilePath] = PathContentsFromBytes([]byte(containerfile))
